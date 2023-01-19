1423d79a02312ce8be59ff752fc4d4dfc0ee47b0
bluebluewaterbottle <122560360+bluebluewaterbottle@users.noreply.github.com>
2023-01-19 13:28:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,36 @@
+let _1234 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    _1234 = randint(0, 5)
+    if (_1234 == 1) {
+        basic.showString("1")
+    } else if (_1234 == 2) {
+        basic.showString("2")
+    } else if (_1234 == 3) {
+        basic.showString("3")
+    } else if (_1234 == 4) {
+        basic.showString("4")
+    } else if (_1234 == 5) {
+        basic.showString("5")
+    } else if (_1234 == 6) {
+    	
+    } else if (_1234 == 7) {
+    	
+    } else if (_1234 == 8) {
+    	
+    } else if (_1234 == 9) {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
